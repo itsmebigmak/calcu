039c9aafb2a07c54c04fe82c0a463cf47464bbe4
itsmebigmak <146430956+itsmebigmak@users.noreply.github.com>
2023-12-13 19:33:33 +0800

    Add files via upload

---
+++ Calcu/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import Calculator from './Calculator';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Calculator</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
---
+++ Calcu/React.js
@@ -0,0 +1,56 @@
+// Calculator.js
+ 
+import React, { useState } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState(0);
+
+  const handleClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleReset = () => {
+    setInput('');
+    setResult(0);
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input));
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('.')}>.</button>
+        <button onClick={() => handleCalculate()}>=</button>
+        <button onClick={() => handleReset()}>C</button>
+        <button onClick={() => handleClick('/')}>/</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
